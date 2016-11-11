822d1c68f8266a1ba0e1d238633bea5a7667d1cb
Bart Langelaan <langelaanbart@gmail.com>
2016-11-11 14:14:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const request = require('request-promise');
+const debug = require('debug')('sawmon:plugin:website-request');
 
 module.exports.websites = {
-    ping: passTrough =>
+    ping: passTrough => {
 
-        request({
+        debug(`Starting request for http://${passTrough.instance.domain}`);
+        
+        return request({
             uri: `http://${passTrough.instance.domain}`,
             resolveWithFullResponse: true,
             time: true
@@ -14,9 +17,13 @@ module.exports.websites = {
             {statusCode: err.error.code == 'ETIMEDOUT' ? 408 : 520}
 
         )).then(requestResponse => {
-
+            
+            debug(`Request for http://${passTrough.instance.domain} done.`);
+            
             passTrough.request = requestResponse;
 
-        })
+        });
+
+    }
 
 };
